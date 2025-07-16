ef7d0670e6a270cc4bd94b32f0a160b351da2676
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2025-07-16 16:39:56 +1200

    Improve implementation.

--- lib/rubocop/socketry/cop/layout/consistent_blank_line_indentation.rb
+++ lib/rubocop/socketry/cop/layout/consistent_blank_line_indentation.rb
@@ -8,36 +8,83 @@ require "rubocop"
 module RuboCop
 	module Cop
 		module Layout
-			# This cop requires that blank lines have the same indentation as the previous non-blank line.
+			# This cop requires that blank lines have the correct indentation based on AST structure.
 			class ConsistentBlankLineIndentation < RuboCop::Cop::Base
 				extend RuboCop::Cop::AutoCorrector
+				include Alignment
+				include RangeHelp
 				
-				MSG = "Blank lines must have the same indentation as the previous non-blank line."
+				MESSAGE = "Blank lines must have the correct indentation."
+				
+				def configured_indentation_width
+					cop_config["IndentationWidth"] || config.for_cop("Layout/IndentationWidth")["Width"] || 1
+				end
+				
+				def configured_indentation_style
+					cop_config["IndentationStyle"] || config.for_cop("Layout/IndentationStyle")["Style"] || "tab"
+				end
+				
+				def indentation(width)
+					case configured_indentation_style
+					when "tab"
+						"\t" * (width * configured_indentation_width)
+					when "space"
+						" " * (width * configured_indentation_width)
+					end
+				end
 				
 				def on_new_investigation
-					source_buffer = processed_source.buffer
-					previous_indent = ""
+					indentation_deltas = build_indentation_deltas
+					current_level = 0
+					
 					processed_source.lines.each_with_index do |line, index|
+						line_number = index + 1
+						
+						# Blank line:
 						if line.strip.empty?
-							current_indent = line
-							unless current_indent == previous_indent
+							expected_indentation = indentation(current_level)
+							if line != expected_indentation
 								add_offense(
-									source_range(source_buffer, index + 1, 0, line.length),
-									message: MSG
+									source_range(processed_source.buffer, line_number, 0, line.length),
+									message: MESSAGE
 								) do |corrector|
 									corrector.replace(
-										source_range(source_buffer, index + 1, 0, line.length),
-										previous_indent
+										source_range(processed_source.buffer, line_number, 0, line.length),
+										expected_indentation
 									)
 								end
 							end
-						else
-							previous_indent = line[/^\s*/]
 						end
+						
+						delta = indentation_deltas[line_number] || 0
+						current_level += delta
 					end
 				end
 				
 				private
+				
+				# Build a hash of line_number => delta (+1 for indent, -1 for dedent)
+				def build_indentation_deltas
+					deltas = Hash.new(0)
+					walk_ast_for_indentation(processed_source.ast, deltas)
+					deltas
+				end
+				
+				def walk_ast_for_indentation(node, deltas)
+					return unless node.is_a?(Parser::AST::Node)
+					
+					case node.type
+					when :block, :hash, :array, :class, :module, :sclass, :def, :defs, :if, :case, :while, :until, :for, :kwbegin
+						if location = node.location
+							deltas[location.line] += 1
+							deltas[location.last_line] -= 1
+						end
+					end
+					
+					node.children.each do |child|
+						walk_ast_for_indentation(child, deltas)
+					end
+				end
 				
 				def source_range(buffer, line, column, length)
 					Parser::Source::Range.new(buffer, buffer.line_range(line).begin_pos + column, buffer.line_range(line).begin_pos + column + length)
--- test/rubocop/socketry/cop/layout/consistent_blank_line_indentation.rb
+++ test/rubocop/socketry/cop/layout/consistent_blank_line_indentation.rb
@@ -3,7 +3,17 @@
 require "rubocop/socketry/cop/layout/consistent_blank_line_indentation"
 
 describe RuboCop::Cop::Layout::ConsistentBlankLineIndentation do
-	let(:cop) {subject.new}
+	# Configure the cop to use 1 tab for indentation
+	let(:config) do
+		RuboCop::Config.new(
+			"Layout/ConsistentBlankLineIndentation" => {
+				"IndentationWidth" => 1,
+				"IndentationStyle" => "tab"
+			}
+		)
+	end
+	
+	let(:cop) {subject.new(config)}
 	
 	with "a blank line with inconsistent indentation" do
 		let(:source) {"def foo\n\tputs 'bar'\n\t\t\n\tputs 'baz'\nend\n"}
@@ -14,7 +24,7 @@ describe RuboCop::Cop::Layout::ConsistentBlankLineIndentation do
 			report = investigator.investigate(processed_source)
 			offenses = report.offenses
 			expect(offenses.length).to be == 1
-			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MSG)
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
 		end
 	end
 	
@@ -26,7 +36,307 @@ describe RuboCop::Cop::Layout::ConsistentBlankLineIndentation do
 			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
 			report = investigator.investigate(processed_source)
 			offenses = report.offenses
+			expect(offenses).to be(:empty?)
+		end
+	end
+	
+	with "a blank line in a block with proper indentation" do
+		let(:source) {"foo do\n\t\n\tbar\nend\n"}
+		
+		it "does not register an offense when blank line is properly indented for block context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
 			expect(offenses.length).to be == 0
+		end
+	end
+	
+	with "a blank line in a block with incorrect indentation" do
+		let(:source) {"foo do\n\n\tbar\nend\n"}
+		
+		it "registers an offense when blank line is not indented for block context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+	
+	with "a blank line in a block with parameters" do
+		let(:source) {"foo do |bar|\n\t\n\tbaz\nend\n"}
+		
+		it "does not register an offense when blank line is properly indented for block with parameters" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+	
+	with "a blank line in a block with parameters and incorrect indentation" do
+		let(:source) { "foo do |bar|\n\n\tbaz\nend\n" }
+
+		it "registers an offense when blank line is not indented for block with parameters" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in an if statement with proper indentation" do
+		let(:source) { "if condition\n\t\n\tputs 'hello'\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for if context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in an if statement with incorrect indentation" do
+		let(:source) { "if condition\n\n\tputs 'hello'\nend\n" }
+
+		it "registers an offense when blank line is not indented for if context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a case statement with proper indentation" do
+		let(:source) { "case value\nwhen 1\n\t\n\tputs 'one'\nwhen 2\n\tputs 'two'\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for case context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a case statement with incorrect indentation" do
+		let(:source) { "case value\nwhen 1\n\n\tputs 'one'\nwhen 2\n\tputs 'two'\nend\n" }
+
+		it "registers an offense when blank line is not indented for case context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a class definition with proper indentation" do
+		let(:source) { "class MyClass\n\t\n\tdef method\n\t\tputs 'hello'\n\tend\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for class context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a class definition with incorrect indentation" do
+		let(:source) { "class MyClass\n\n\tdef method\n\t\tputs 'hello'\n\tend\nend\n" }
+
+		it "registers an offense when blank line is not indented for class context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a module definition with proper indentation" do
+		let(:source) { "module MyModule\n\t\n\tdef method\n\t\tputs 'hello'\n\tend\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for module context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a module definition with incorrect indentation" do
+		let(:source) { "module MyModule\n\n\tdef method\n\t\tputs 'hello'\n\tend\nend\n" }
+
+		it "registers an offense when blank line is not indented for module context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a begin block with proper indentation" do
+		let(:source) { "begin\n\t\n\tputs 'hello'\nrescue => e\n\tputs e\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for begin context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a begin block with incorrect indentation" do
+		let(:source) { "begin\n\n\tputs 'hello'\nrescue => e\n\tputs e\nend\n" }
+
+		it "registers an offense when blank line is not indented for begin context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a while loop with proper indentation" do
+		let(:source) { "while condition\n\t\n\tputs 'hello'\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for while context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a while loop with incorrect indentation" do
+		let(:source) { "while condition\n\n\tputs 'hello'\nend\n" }
+
+		it "registers an offense when blank line is not indented for while context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in an until loop with proper indentation" do
+		let(:source) { "until condition\n\t\n\tputs 'hello'\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for until context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in an until loop with incorrect indentation" do
+		let(:source) { "until condition\n\n\tputs 'hello'\nend\n" }
+
+		it "registers an offense when blank line is not indented for until context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a for loop with proper indentation" do
+		let(:source) { "for item in items\n\t\n\tputs item\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for for context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a for loop with incorrect indentation" do
+		let(:source) { "for item in items\n\n\tputs item\nend\n" }
+
+		it "registers an offense when blank line is not indented for for context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in a rescue block with proper indentation" do
+		let(:source) { "begin\n\tputs 'hello'\nrescue => e\n\t\n\tputs e\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for rescue context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in a rescue block with incorrect indentation" do
+		let(:source) { "begin\n\tputs 'hello'\nrescue => e\n\n\tputs e\nend\n" }
+
+		it "registers an offense when blank line is not indented for rescue context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
+		end
+	end
+
+	with "a blank line in an ensure block with proper indentation" do
+		let(:source) { "begin\n\tputs 'hello'\nensure\n\t\n\tputs 'cleanup'\nend\n" }
+
+		it "does not register an offense when blank line is properly indented for ensure context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 0
+		end
+	end
+
+	with "a blank line in an ensure block with incorrect indentation" do
+		let(:source) { "begin\n\tputs 'hello'\nensure\n\n\tputs 'cleanup'\nend\n" }
+
+		it "registers an offense when blank line is not indented for ensure context" do
+			processed_source = RuboCop::ProcessedSource.new(source, RUBY_VERSION.to_f)
+			investigator = RuboCop::Cop::Commissioner.new([cop], [], raise_error: true)
+			report = investigator.investigate(processed_source)
+			offenses = report.offenses
+			expect(offenses.length).to be == 1
+			expect(offenses.first.message).to be(:include?, RuboCop::Cop::Layout::ConsistentBlankLineIndentation::MESSAGE)
 		end
 	end
 end 
